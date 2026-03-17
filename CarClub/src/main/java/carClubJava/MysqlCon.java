bb70183e0331ef423ca22b38c21d41c6e947c801
Vincent Cheng <93815609+vincentc168777@users.noreply.github.com>
2026-03-17 11:25:13 -0700

    separate java code and jsp

--- CarClub/src/main/java/carClubJava/MysqlCon.java
+++ CarClub/src/main/java/carClubJava/MysqlCon.java
@@ -2,24 +2,38 @@ package carClubJava;
 
 import java.sql.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MysqlCon {
-    public static void main(String[] args) {
+
+    public static List<String[]> getCars() {
+        List<String[]> cars = new ArrayList<>();
+        String user = "root";
+        String password = "root";
+
         try {
-        	Class.forName("com.mysql.jdbc.Driver");
-        	Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/cheng?autoReconnect=true&useSSL=false","root","root");
-        	//here cheng is database name, root is username and password
-        	Statement stmt=con.createStatement();
-        	ResultSet rs=stmt.executeQuery("select * from Cars");
-        	while(rs.next())
-        	System.out.println(rs.getString(1)+" "+rs.getString(2));
-        	rs.close();
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection con = DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/cheng?autoReconnect=true&useSSL=false",
+                user, password
+            );
+
+            Statement stmt = con.createStatement();
+            ResultSet rs = stmt.executeQuery("SELECT * FROM Cars");
+
+            while (rs.next()) {
+                cars.add(new String[]{ rs.getString(1), rs.getString(2) });
+            }
+
+            rs.close();
             stmt.close();
-        	con.close();
-            System.out.println("Connection closed.");
-            
+            con.close();
+
         } catch (Exception e) {
-            System.out.println(e.getMessage());
             e.printStackTrace();
         }
+
+        return cars;
     }
 }
--- CarClub/src/main/webapp/3-tier.jsp
+++ CarClub/src/main/webapp/3-tier.jsp
@@ -1,9 +1,10 @@
-<%@ page import="java.sql.*"%>
+<%@ page import="carClubJava.MysqlCon, java.util.List" %>
 <html>
 <head>
   <title>Car Club</title>
 </head>
 <body>
+
 <img src="<%= request.getContextPath() %>/images/dom.jpg" alt="A descriptive text for the image">
 <h1>Welcome to Car Club</h1>
 
@@ -12,34 +13,16 @@
     <td>Car ID</td>
     <td>Name</td>
   </tr>
-    <%
-     String db = "Car Club's";
-        String user; // assumes database name is the same as username
-          user = "root";
-        String password = "root";
-        try {
-            java.sql.Connection con;
-            Class.forName("com.mysql.jdbc.Driver");
+  <%
+    List<String[]> cars = MysqlCon.getCars();
+    for (String[] car : cars) {
+  %>
+  <tr>
+    <td><%= car[0] %></td>
+    <td><%= car[1] %></td>
+  </tr>
+  <% } %>
+</table>
 
-            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/cheng?autoReconnect=true&useSSL=false",user, password);
-
-            out.println(db + " database successfully opened.<br/><br/>");
-
-            out.println("Initial entries in table \"Cars\": <br/>");
-
-            Statement stmt = con.createStatement();
-
-            ResultSet rs = stmt.executeQuery("SELECT * FROM Cars");
-
-            while (rs.next()) {
-         out.println("<tr>" + "<td>" +  rs.getString(1) + "</td>"+ "<td>" +  rs.getString(2) + "</td>");
-            }
-            rs.close();
-            stmt.close();
-            con.close();
-        } catch(SQLException e) {
-            out.println("SQLException caught: " + e.getMessage());
-        }
-    %>
 </body>
 </html>
